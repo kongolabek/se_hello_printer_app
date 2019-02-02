199ff8dc40a5d3b6ce9bad0886063a216afba2a3
kongolabek <kongolabek@users.noreply.github.com>
2019-02-02 13:46:09 +0100

    k

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Konrad:)"
 msg = "Hello World!"
 
 @app.route('/')
